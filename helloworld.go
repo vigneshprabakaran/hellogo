54136f578a80792ce3e610aea421189abc39a648
Vignesh Prabakaran <vignesh.prabakaran@gmail.com>
2014-06-14 19:18:36 -0400

    added some code

--- helloworld.go
+++ helloworld.go
@@ -2,10 +2,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Test")
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println(os.Open("filename"))
+
+	conn, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		// handle error
+		fmt.Println("error")
+	}
+
+	//fmt.Println(conn,)
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
 }
