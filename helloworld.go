8fbd20a39d41b34f8ec6dd474f41d14663802b8b
Vignesh Prabakaran <vignesh.prabakaran@gmail.com>
2014-06-14 20:30:41 -0400

    added more go tour example code

--- helloworld.go
+++ helloworld.go
@@ -4,6 +4,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"net"
 	"os"
 	"time"
@@ -28,4 +29,32 @@ func main() {
 	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 	status, err := bufio.NewReader(conn).ReadString('\n')
 	fmt.Println(status)
+
+	fmt.Printf("Now you have %g problems.\n",
+		math.Nextafter(4, 1))
+
+	//function calls in go
+	fmt.Println(" 3 + 5 is ", add(3, 5))
+
+	x := "Hello"
+	y := "World"
+
+	//mutiple returns example
+	x, y = swap(x, y)
+
+	// string interpolation - formatted printing
+	fmt.Printf(" Swapped x: Hello , y: World as x:%s  and y:%s ", x, y)
+
+}
+
+// add is a function to add 2 numbers
+func add(x int, y int) int {
+
+	return x + y
+}
+
+// swap is a function to swap 2 strings
+func swap(x, y string) (string, string) {
+
+	return y, x
 }
